8746fa37485ed4674a1406a5d74bae13cedbd254
matt <matt@showworxmedia.com>
2026-02-07 16:48:41 +0000

    File changes

--- src/components/pipeline/EnhancedPipelineCard.jsx
+++ src/components/pipeline/EnhancedPipelineCard.jsx
@@ -138,13 +138,7 @@ export default function EnhancedPipelineCard({ item, job, onStatusChange, onLaun
             </Button>
           )}
 
-          {/* Enrich if needed */}
-          {enrichMutation.isPending && (
-            <div className="text-xs text-blue-600 flex items-center gap-1.5 px-3 py-2 bg-blue-50 rounded-lg">
-              <Loader2 className="w-3 h-3 animate-spin" />
-              <span>Enriching contacts...</span>
-            </div>
-          )}
+
         </div>
       </div>
 
